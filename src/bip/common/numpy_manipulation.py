1e4dd427245b3e3bb3e379038c2929b5c9f5de78
Nick <nicholas.milford@us.af.mil>
2024-12-30 16:30:46 -0700

    use numba for faster bit manipulation

--- src/bip/common/numpy_manipulation.py
+++ src/bip/common/numpy_manipulation.py
@@ -4,8 +4,8 @@ def rolling_window(array, window):
     shape = array.shape[:-1] + (array.shape[-1] - window + 1, window)
     strides = array.strides + (array.strides[-1],)
     return np.lib.stride_tricks.as_strided(array, shape=shape, strides=strides)
-    
-           
+
+
 def find_subarray_indexes(array, subarray):
     temp = rolling_window(array, len(subarray))
     result = np.where(np.all(temp == subarray, axis=1))
